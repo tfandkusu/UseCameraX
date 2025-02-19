47f15416ed39276d65a8f475e757354721839da6
Haruhiko Takada <haruhiko.takada@gmail.com>
2025-02-20 02:55:32 +0900

    権限取得

--- app/src/main/java/com/tfandkusu/camera/MainActivity.kt
+++ app/src/main/java/com/tfandkusu/camera/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.tfandkusu.camera
 
+import android.Manifest
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            // カメラの権限が許可された時の処理
+            Toast.makeText(this, getString(R.string.camera_permission_granted), Toast.LENGTH_SHORT).show()
+        } else {
+            // カメラの権限が拒否された時の処理
+            Toast.makeText(this, getString(R.string.camera_permission_denied), Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +32,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // カメラのパーミッションをチェック
+        requestPermissionLauncher.launch(Manifest.permission.CAMERA)
     }
 }
