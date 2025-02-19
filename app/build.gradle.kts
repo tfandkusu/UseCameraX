c8125d8064a9b067d3e1a1114e77e13ff0640074
Haruhiko Takada <haruhiko.takada@gmail.com>
2025-02-20 02:51:15 +0900

    CameraX ライブラリ追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
 }
